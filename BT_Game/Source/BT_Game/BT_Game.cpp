b2838996b730b2ad98b835ed28ab1c493dd6a472
SlayerPunk <slayerpunk@mail.ru>
2018-10-17 00:10:45 +0300

    BT - 01 Landscape & TankModel
    Creating landscape about 1x1 km with low poly, create tank model.

---
+++ BT_Game/Source/BT_Game/BT_Game.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BT_Game.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BT_Game, "BT_Game" );
---
+++ BT_Game/Source/BT_Game/BT_GameGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BT_Game.h"
+#include "BT_GameGameModeBase.h"
+
+
+
+
