0880406f1795d287b983395b1a3c094e49cd1e6b
SlayerPunk <slayerpunk@mail.ru>
2018-10-26 22:57:40 +0300

    BT - 10 Add AIController and Looking for PlayerController by AiController

---
+++ BT_Game/Source/BT_Game/Private/TankAIController.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BT_Game.h"
+#include "TankAIController.h"
+#include "Classes/GameFramework/Actor.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
+#include "Runtime/Engine/Classes/GameFramework/PlayerController.h"
+#include "Runtime/Engine/Classes/Components/PrimitiveComponent.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	ATank* ControlledAiTank = GetControlledTank();
+	if (!ControlledAiTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("No tank possessed to TanKAiController"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s possessed to AiController"), *ControlledAiTank->GetName());
+	}
+
+	ATank* PlayerControlledTank = GetPlayerTank();
+	if (!PlayerControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("AiController can't find player tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI controller found player  %s "), *PlayerControlledTank->GetName());
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+ATank* ATankAIController::GetPlayerTank() const
+{
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
