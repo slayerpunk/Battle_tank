756c0d32d183b858a15a384121bfd532c3564113
SlayerPunk <slayerpunk@mail.ru>
2019-02-03 22:50:16 +0300

    BT23 - Move the Barrel

--- BT_Game/Source/BT_Game/Private/TankAimingComponent.cpp
+++ BT_Game/Source/BT_Game/Private/TankAimingComponent.cpp
@@ -80,5 +80,5 @@ void  UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
 	auto AimAsRotator = AimDirection.Rotation();
 	auto DifferenceRotator = AimAsRotator - BarrelRotator;
-	Barrel->Elevate(5); //TODO remove magic number 
+	Barrel->Elevate(DifferenceRotator.Pitch); //TODO remove magic number 
 }
--- BT_Game/Source/BT_Game/Private/TankBarrel.cpp
+++ BT_Game/Source/BT_Game/Private/TankBarrel.cpp
@@ -3,10 +3,16 @@
 #include "BT_Game.h"
 #include "TankBarrel.h"
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
-
-	UE_LOG(LogTemp, Warning, TEXT("Barrel Elevate at speed %f"), DegreesPerSecond);
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f: Barrel Elevate at speed %f"), Time, RelativeSpeed);
+	auto ElevationChange = GetWorld()->DeltaTimeSeconds * RelativeSpeed * MaxDegreesPerSecond;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	
+	SetRelativeRotation(FRotator (FMath::Clamp<float>(RawNewElevation, MinElevation, MaxElevation), .0f, .0f));
+	 
 }
 
 
